2a1f9969d8bca277a61dddec37aa4797117fa8c1
王晓光 <xiaoguang15@staff.weibo.com>
2022-04-20 18:21:49 +0800

    update

--- project.clj
+++ project.clj
@@ -3,13 +3,18 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring "1.9.5"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
                  [cheshire "5.10.2"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [mysql/mysql-connector-java "5.1.26"]
+                 [korma "0.4.3"]
                  [ring/ring-json "0.5.1"]]
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler scratch-app.handler/app}
-  :profiles {:dev {
-                   :dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring/ring-mock "0.3.2"]]
-                   }})
+  :ring {:handler scratch-app.core/app}
+  :profiles {:dev {:dependencies [[ring/ring-mock "0.3.2"]]
+                   }}
+  :main ^:sikp-aot scratch-app.core
+  :aot [scratch-app.core]
+  :repl-options {:init-ns scratch-app.core})
